fb62f1bacffd3f3e797d5842efaf33ef1aa8ae0e
Professor Claudio R. Silva <133040671+ProfessorClaudioSilva@users.noreply.github.com>
2025-07-01 16:34:53 -0300

    sasfasfa

--- script.js
+++ script.js
@@ -13,3 +13,6 @@ console.log(linguagnes[1]); // HTML
 console.log(linguagnes[2]); // Python
 console.log(linguagnes[3]); // C
 console.log(linguagnes[4]); // C++
+
+const lista = document.querySelector("#lista");
+lista.textContent = lingugens[0];
